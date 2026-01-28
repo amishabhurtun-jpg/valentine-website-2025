bdd52bea0a152d4a52fae784e0809799f7ad5d64
amishabhurtun-jpg <amisha.bhurtun@umail.uom.ac.mu>
2026-01-28 11:01:43 +0400

    Update config.js
    Trial basis

--- config.js
+++ config.js
@@ -5,12 +5,13 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "DUMDUM",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
-
+    pageTitle:" The most awaited Question of the Year"
+             : " Will you be My Valentine? 💝",
+            
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
@@ -29,11 +30,11 @@ const CONFIG = {
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
+            startText: "This much only?",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -49,8 +50,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! I'm the luckiest person in the world to have you in my life! 🎉💝💖💝💓",
+        message: "A big warm hug and a huge kiss for you DUMDUM!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
